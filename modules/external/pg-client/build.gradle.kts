29827c3194656ee0061818d70d7509ac094fc965
i love milkiss <shykse@naver.com>
2025-10-05 17:16:06 +0900

    chore: add PG test config and testing dependencies

--- modules/external/pg-client/build.gradle.kts
+++ modules/external/pg-client/build.gradle.kts
@@ -10,4 +10,12 @@ dependencies {
     implementation(projects.modules.application)
     implementation(projects.modules.domain)
     implementation(libs.spring.boot.starter.web)
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
